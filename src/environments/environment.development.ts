6e7408a1627079e44dd8c05a39773f6be4245bd2
codewithah <codewithah@gmail.com>
2024-12-26 14:27:56 +0330

    add environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  appwrite: {
+    endpoint: 'https://8080-appwrite-integrationfor-ucwihch1vgz.ws-eu117.gitpod.io/v1',
+    projectId: 'angular-love-new-website', // Replace with your Appwrite project ID
+  },
+};
